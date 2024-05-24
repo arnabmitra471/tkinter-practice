34ba74403a70d4a8c4507cb03b05d140c1f018a1
Arnab Mitra <arnabmitra471@gmail.com>
2024-05-24 13:37:49 +0530

    writing the travel data submitted by user to a file

--- tut11.py
+++ tut11.py
@@ -5,7 +5,13 @@ from tkinter import *
 root = Tk()
 root.title("Travel form with grid layout")
 
-root.geometry("644x344")
+root.geometry("644x444")
+def get_data():
+    with open("traveldata.txt","a") as file:
+        file.write(f"name: {name_value.get()} phone: {phone_value.get()} gender: {gender_value.get()} emergency contact: {enmergency_value.get()} payment mode: {payment_mode_value.get()} food service chosen: {food_service_value.get()}")
+    
+    print("Written to file successfully")
+
 # Creating the Heading label
 title_label = Label(root,text="Welcome to anala tours",font="comicsansms 13 bold",pady=15)
 
@@ -41,18 +47,27 @@ food_service_value = IntVar()
 
 name_entry = Entry(root,textvariable=name_value)
 phone_entry = Entry(root,textvariable=phone_value)
-gender_entry = Entry(root,textvariable=phone_value)
-emergency_entry = Entry(root,textvariable=phone_value)
-payment_mode_entry = Entry(root,textvariable=phone_value)
+gender_entry = Entry(root,textvariable=gender_value)
+emergency_entry = Entry(root,textvariable=enmergency_value)
+payment_mode_entry = Entry(root,textvariable=payment_mode_value)
+
+# Checkbutton for food service
+
+food_service_check = Checkbutton(root,text="Want to prebook your meals",fg="orangered",variable=food_service_value)
+
+# Creating a button packing it and assigning it a command
+submit_btn = Button(root,text="Submit to anala tours",bg="salmon",fg="white",command=get_data)
+submit_btn.grid(row=7,column=2)
 
 
 # Packing the entries
 
-name_entry.grid(row=1,column=2)
-phone_entry.grid(row=2,column=2)
-gender_entry.grid(row=3,column=2)
-emergency_entry.grid(row=4,column=2)
-payment_mode_entry.grid(row=5,column=2)
+name_entry.grid(row=1,column=2,pady=15)
+phone_entry.grid(row=2,column=2,pady=15)
+gender_entry.grid(row=3,column=2,pady=15)
+emergency_entry.grid(row=4,column=2,pady=15)
+payment_mode_entry.grid(row=5,column=2,pady=15)
+food_service_check.grid(row=6,column=2,pady=15)
 
 # Starting the GUI
 
